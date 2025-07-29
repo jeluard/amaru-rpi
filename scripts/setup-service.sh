d2631ccc9fe3378add16212bc34ad25826770776
jeluard <jeluard@users.noreply.github.com>
2025-07-29 12:14:52 +0200

    feat: added amaru.service
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

---
+++ scripts/setup-service.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cp "$SCRIPT_DIR/../conf/amaru.service" /etc/systemd/system/amaru.service
+sudo systemctl enable amaru.service
+sudo systemctl start amaru.service
